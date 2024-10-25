4c5bed8660de884b67c555b1d26d4ccce6b89268
Kosi Mensah <kmensah@usf.edu>
2024-10-25 01:03:32 -0400

    Calculate and update total price dynamically based on product and quantity

--- main.js
+++ main.js
@@ -8,3 +8,14 @@ const orderSummary = document.getElementById('order-summary');
 
 productSelector.addEventListener('change', calculateTotalPrice);
 quantityInput.addEventListener('input', calculateTotalPrice);
+
+// Task 3: Calculate Total Price 
+
+function calculateTotalPrice() { 
+    const productPrice = (productSelector.value);
+    const quantity = (quantityInput.value);
+    const totalPrice = productPrice * quantity; // based on quantity, total price will be updated
+    
+    totalPriceElement.textContent = totalPrice
+}
+
